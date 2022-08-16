3e3617ffb754ce9031b1c5fd53c7e5fd72427136
minna <yakubuaminat94@gmail.com>
2022-08-16 23:12:43 +0100

    feat: image list component

--- app/components/ImageInput.js
+++ app/components/ImageInput.js
@@ -1,17 +1,54 @@
-import React from "react";
-import { View, StyleSheet, Image } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import * as ImagePicker from "expo-image-picker";
+import * as React from "react";
+import { Alert, Image, StyleSheet, TouchableWithoutFeedback, View } from "react-native";
 
 import colors from "../config/colors";
 
-const ImageInput = ({ imageUri }) => {
+const ImageInput = ({ imageUri, onChangeImage}) => {
+
+  React.useEffect(() => {
+    requestPermission();
+  }, []);
+
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestCameraPermissionsAsync();
+    if (!granted) alert("You need permission");
+  };
+
+  const handlePress = () => {
+    if (!imageUri) selectImage();
+    else Alert.alert('Delete', 'Are you sure ypu want to delete this image?', [
+      {text: 'Yes', onPress: () => onChangeImage(null)},
+      {text: 'No'}
+    ])
+  };
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 0.5
+      });
+      if (!result.cancelled) onChangeImage(result.uri);
+    } catch (error) {
+      console.log("error reading image");
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      {!imageUri && (
-        <MaterialCommunityIcons color={colors.medium} name="camera" size={40} />
-      )}
-      {imageUri && <Image source={{ uri: imageUri }} style={styles.image} />}
-    </View>
+    <TouchableWithoutFeedback onPress={handlePress}>
+      <View style={styles.container}>
+        {!imageUri && (
+          <MaterialCommunityIcons
+            color={colors.medium}
+            name="camera"
+            size={40}
+          />
+        )}
+        {imageUri && <Image source={{ uri: imageUri }} style={styles.image} />}
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
@@ -23,7 +60,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     height: 100,
     width: 100,
-    overflow: 'hidden'
+    overflow: "hidden",
   },
   image: {
     height: "100%",
